ba7ba69c35b437e5cd0b97700ca91a5041bd5bec
vishal <vishalgajera101@gmail.com>
2023-05-07 19:20:09 +0530

    fix build issue

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React,{ useEffect } from "react";
-import $ from 'jquery'; 
 import dynamic from "next/dynamic";
 import Seo from "../components/common/Seo";
 import Home2 from "../components/home-2";
